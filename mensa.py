b5647e58680758d32e8a524a89e571c6eb3abccc
Luka Steinbach <luka.steinbach@gmx.de>
2019-10-06 18:16:02 +0200

    Added printMeals function

--- mensa.py
+++ mensa.py
@@ -4,17 +4,22 @@ from datetime import date
 
 URL = "https://www.imensa.de/freiburg/mensa-flugplatz/{}.html"
 
-print("\n" + "-" * 50)
-print("Speiseplan für Mensa Flugplatz:\n")
-for i in range(3):
-    day = ["montag", "dienstag", "mittwoch", "donnerstag", "freitag", "samstag", "sonntag"][(date.today().weekday() + i) % 7]
-    r = requests.get(URL.format(day))
-    soup = bs(r.content, 'html.parser')
-    meals = soup.find_all("p", class_ = "aw-meal-description")
-    print(["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"][(date.today().weekday() + i) % 7] + (" (heute):" if i == 0 else ":"))
-    if len(meals) == 0: print("    Heute keine Essensausgabe")
-    for j, meal in enumerate(meals):
-        if (meal.text != "Heute keine Essensausgabe"): print("  Essen " + str(j + 1) + ":")
-        print("    " + meal.get_text(separator="\n    "))
+def printMeals(days):
+    print("\n" + "-" * 50)
+    print("Speiseplan für Mensa Flugplatz:\n")
+    for i in range(days):
+        day = ["montag", "dienstag", "mittwoch", "donnerstag", "freitag", "samstag", "sonntag"][(date.today().weekday() + i) % 7]
+        r = requests.get(URL.format(day))
+        soup = bs(r.content, 'html.parser')
+        meals = soup.find_all("p", class_ = "aw-meal-description")
+        print(["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"][(date.today().weekday() + i) % 7] + (" (heute):" if i == 0 else ":"))
+        if len(meals) == 0: print("    Heute keine Essensausgabe")
+        for j, meal in enumerate(meals):
+            if (meal.text != "Heute keine Essensausgabe"): print("  Essen " + str(j + 1) + ":")
+            print("    " + meal.get_text(separator="\n    "))
 
-print("-" * 50, "\n", sep="")
+    print("-" * 50, "\n", sep="")
+
+
+if __name__ == "__main__":
+    printMeals(3)
